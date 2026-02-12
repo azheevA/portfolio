08cb5df9af06174fc04a92668081e95a306872a1
azheevA <sazheev@gmail.com>
2026-02-13 01:07:09 +0800

    add logo-showcase

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import NavBar from "./components/Navbar";
 import Hero from "./sections/Hero";
+import LogoShowcase from "./sections/LogoShowcase";
 import AppShowcase from "./sections/ShowcaseSection";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <NavBar />
       <Hero />
       <AppShowcase />
+      <LogoShowcase />
     </>
   );
 }
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -32,4 +32,39 @@ const counterItems = [
   { value: 26, suffix: "+", label: "Завершенных проектов" },
   { value: 87, suffix: "%", label: "Положительных отзывов от клиентов" },
 ];
-export { navLinks, words, counterItems };
+const logoIconsList = [
+  {
+    imgPath: "/images/logos/company-logo-1.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-2.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-3.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-4.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-5.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-6.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-7.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-8.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-9.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-10.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-11.png",
+  },
+];
+export { navLinks, words, counterItems, logoIconsList };
